f52505e8952113cc8595f96088b07f411ec5d2e4
Doug McGeehan <djmvfb@mst.edu>
2017-01-08 14:31:50 -0600

    initial class with migration

--- stacktrack/models.py
+++ stacktrack/models.py
@@ -2,4 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+	when = models.DateTimeField('date created', auto_now_add=True)
+
+
+class Fineness(models.Model):
+	multiplier = models.FloatField(null=False, default=1.0)
+	friendly_name = models.CharField(max_length=80, null=False)
+
+	class Meta:
+		verbose_name_plural = 'finenesses'
+
+	def __str__(self):
+		return '{friendly_name}: x{multiplier} multiplier'.format(
+			multiplier=self.multiplier,
+			friendly_name=self.friendly_name,
+		)
