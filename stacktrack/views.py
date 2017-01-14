efaa630bf630308a6802bb6c2d9fc2db1ccee27b
Doug McGeehan <djmvfb@mst.edu>
2017-01-13 23:10:57 -0600

    calculating parameters and displaying it nicely

--- stacktrack/views.py
+++ stacktrack/views.py
@@ -22,10 +22,39 @@ def dashboard(request):
 	user = User.objects.all()[0]
 	stack_entries = StackEntry.objects.filter(owner=user)\
 		.order_by('-purchase__timestamp')
+
+	# calculate aggregate attributes
+	purchases = 0
+	sales = 0
+	profits = 0
+	weight = 0
+	qty = 0
+	for entry in stack_entries:
+		purchase_price = entry.bought_for.amount
+		purchases += purchase_price
+		if not entry.sale:
+			weight += entry.ingot.mass.convert_to_ozt()
+			qty += 1
+
+		else:
+			# this ingot was sold
+			sale_price = entry.sold_for.amount
+			sales += sale_price
+			profits += (sale_price - purchase_price)
+
+	cost_per_ozt = float(purchases - sales)/float(weight)
+
 	return render(request, 'stack.html', {
-		'stack_entries': stack_entries
+		'stack_entries': stack_entries,
+		'cost_per_ozt': cost_per_ozt,
+		'weight': weight,
+		'qty': qty,
+		'purchases': purchases,
+		'sales': sales,
+		'profits': profits,
 	})
 	#return render(request, 'dashboard.html')
+
 
 from .batch import main
 def batch(request):
